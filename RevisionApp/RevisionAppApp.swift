b3c536b81b5f1cf5e7211aca04a6337f2dde16a4
JoeAZTaylor <56205926+JoeAZTaylor@users.noreply.github.com>
2024-08-12 20:17:34 +0100

    Initial Commit

---
+++ RevisionApp/RevisionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevisionAppApp.swift
+//  RevisionApp
+//
+//  Created by Joe Taylor on 12/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct RevisionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
