1271f02d5226f211abba3556a35d10fedd8c72a8
JoeAZTaylor <joetaylor509@yahoo.com>
2024-09-04 10:34:08 +0100

    changes to background color

--- RevisionApp/QuizContent/QuizView/ContentView.swift
+++ RevisionApp/QuizContent/QuizView/ContentView.swift
@@ -12,46 +12,50 @@ struct ContentView: View {
     @ObservedObject var viewModel: QuizViewModel
     
     var body: some View {
-        VStack {
-            if viewModel.questionModel.isEmpty {
-                Text("Loading questions...")
-                    .font(.title)
-            } else {
-                VStack {
-                    HStack {
-                        Text(viewModel.questionModel[viewModel.currentQuestion].questionText)
-                            .font(.title)
-                            .bold()
-                            .multilineTextAlignment(.leading)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.1), Color.purple.opacity(0.1)]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea(.all)
+            VStack {
+                if viewModel.questionModel.isEmpty {
+                    Text("Loading questions...")
+                        .font(.title)
+                } else {
+                    VStack {
+                        HStack {
+                            Text(viewModel.questionModel[viewModel.currentQuestion].questionText)
+                                .font(.title)
+                                .bold()
+                                .multilineTextAlignment(.leading)
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                        
                         Spacer()
-                    }
-                    .padding(.horizontal)
-                    
-                    Spacer()
-                    
-                    ForEach(0..<viewModel.questionModel[viewModel.currentQuestion].possibleAnswers.count) { answerIndex in
-                        Button {
-                            let selectedAnswer = viewModel.questionModel[viewModel.currentQuestion].possibleAnswers[answerIndex]
-                            if selectedAnswer.isAnswer {
-                                viewModel.answerIsCorrect()
-                            } else {
-                                viewModel.answerIsIncorrect()
+                        
+                        ForEach(0..<viewModel.questionModel[viewModel.currentQuestion].possibleAnswers.count) { answerIndex in
+                            Button {
+                                let selectedAnswer = viewModel.questionModel[viewModel.currentQuestion].possibleAnswers[answerIndex]
+                                if selectedAnswer.isAnswer {
+                                    viewModel.answerIsCorrect()
+                                } else {
+                                    viewModel.answerIsIncorrect()
+                                }
+                                viewModel.answerPressed()
+                            } label: {
+                                HStack {
+                                    Text(viewModel.questionModel[viewModel.currentQuestion].possibleAnswers[answerIndex].optionText)
+                                    Spacer()
+                                }
+                                .modifier(AnswerButton())
                             }
-                            viewModel.answerPressed()
-                        } label: {
-                            HStack {
-                                Text(viewModel.questionModel[viewModel.currentQuestion].possibleAnswers[answerIndex].optionText)
-                                Spacer()
-                            }
-                            .modifier(AnswerButton())
                         }
                     }
-                }
-                .padding()
-                .sheet(isPresented: $viewModel.showingCorrectAnswerSheet) {
-                    AnswerSheetView(viewModel: viewModel)
-                        .presentationDetents([.fraction(0.3)])
-                        .interactiveDismissDisabled()
+                    .padding()
+                    .sheet(isPresented: $viewModel.showingCorrectAnswerSheet) {
+                        AnswerSheetView(viewModel: viewModel)
+                            .presentationDetents([.fraction(0.3)])
+                            .interactiveDismissDisabled()
+                    }
                 }
             }
         }
