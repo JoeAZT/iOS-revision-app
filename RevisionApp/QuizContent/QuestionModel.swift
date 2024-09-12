c43c33185e877d0a586eda429706cdb88085c7cd
JoeAZTaylor <joetaylor509@yahoo.com>
2024-09-12 23:46:54 +0100

    new model added

--- RevisionApp/QuizContent/QuestionModel.swift
+++ RevisionApp/QuizContent/QuestionModel.swift
@@ -16,3 +16,9 @@ struct QuestionModel: Codable {
     let questionText: String
     let possibleAnswers: [OptionModel]
 }
+
+struct QuizStats: Codable {
+    let quizName: String
+    var questions: [QuestionModel]
+    var bestScore: Int = 0
+}
