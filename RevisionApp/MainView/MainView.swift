fb71b6d2b6a4b9486b4e8e237bf3f33b49b0dc9a
JoeAZTaylor <joetaylor509@yahoo.com>
2024-09-15 19:43:37 +0100

    scroll added

--- RevisionApp/MainView/MainView.swift
+++ RevisionApp/MainView/MainView.swift
@@ -17,27 +17,27 @@ struct MainView: View {
                 LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.1), Color.purple.opacity(0.1)]), startPoint: .topLeading, endPoint: .bottomTrailing)
                     .ignoresSafeArea(.all)
                 VStack {
-                    ForEach(viewModel.quizzes, id: \.self) { quiz in
-                        let bestScore = scoreManager.getScore(for: quiz)
-                        let totalQuestions = scoreManager.getTotalQuestions(for: quiz)
-                        let scorePercentage = totalQuestions > 0 ? (Double(bestScore) / Double(totalQuestions)) * 100 : 0
-                        
-                        Button {
-                            viewModel.didTapNavigateToQuiz(selectedQuiz: quiz)
-                        } label: {
-                            HStack {
-                                Text(quiz)
-                                Spacer()
-                                
-                                // Determine trophy color based on score percentage
-                                Image(systemName: "trophy.fill")
-                                    .foregroundColor(viewModel.trophyColor(for: scorePercentage))
+                    ScrollView {
+                        ForEach(viewModel.quizzes, id: \.self) { quiz in
+                            let bestScore = scoreManager.getScore(for: quiz)
+                            let totalQuestions = scoreManager.getTotalQuestions(for: quiz)
+                            let scorePercentage = totalQuestions > 0 ? (Double(bestScore) / Double(totalQuestions)) * 100 : 0
+                            
+                            Button {
+                                viewModel.didTapNavigateToQuiz(selectedQuiz: quiz)
+                            } label: {
+                                HStack {
+                                    Text(quiz)
+                                    Spacer()
+                                    
+                                    // Determine trophy color based on score percentage
+                                    Image(systemName: "trophy.fill")
+                                        .foregroundColor(viewModel.trophyColor(for: scorePercentage))
+                                }
+                                .modifier(MainButtonStyle())
                             }
-                            .modifier(MainButtonStyle())
                         }
                     }
-
-                    Spacer()
                 }
                 .padding(.horizontal)
                 .navigationDestination(for: Route.self) { $0 }
