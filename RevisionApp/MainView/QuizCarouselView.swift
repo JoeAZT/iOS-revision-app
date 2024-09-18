5015a039228e4684195e07b3be7d9119173f146c
JoeAZTaylor <joetaylor509@yahoo.com>
2024-09-18 11:53:02 +0100

    Clean up of carousel

--- RevisionApp/MainView/QuizCarouselView.swift
+++ RevisionApp/MainView/QuizCarouselView.swift
@@ -8,30 +8,39 @@
 import SwiftUI
 
 struct QuizCarouselView: View {
+    var colors: [Color] = [.red, .pink, .purple, .blue, .green, .yellow, .orange]
     var quizzess: [String]
+    
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack {
-                ForEach(quizzess, id: \.self) { quiz in
+                ForEach(Array(zip(quizzess.indices, quizzess)), id: \.1) { index, quiz in
                     VStack {
                         Image(systemName: "iphone.gen2")
                             .resizable()
                             .scaledToFit()
-                            .frame(width: 80, height: 80)
+                            .frame(width: 60, height: 60)
                             .padding()
-                            .background(RadialGradient(colors: [.red, .orange], center: .bottomLeading, startRadius: 40, endRadius: 200))
+                            .background(LinearGradient(gradient: Gradient(colors: getGradientColors(for: index)), startPoint: .topLeading, endPoint: .bottomTrailing))
                             .cornerRadius(20)
                             .shadow(radius: 3)
                         Text(quiz)
                     }
+                    .padding(.trailing, 10)
                 }
             }
+            .padding(.leading, 20)
         }
+        .frame(height: 120)
+        .padding(.top, 10)
     }
-}
-
-#Preview {
-    QuizCarouselView(quizzess: ["Combine", "SwiftUI", "General Knowledge", "Architecture"])
+    
+    private func getGradientColors(for index: Int) -> [Color] {
+        let colorCount = colors.count
+        let firstColor = colors[index % colorCount]
+        let secondColor = colors[(index + 1) % colorCount]
+        return [firstColor, secondColor]
+    }
 }
 
 
