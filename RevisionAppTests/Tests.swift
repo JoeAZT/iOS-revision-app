c90443ecd9e900ff25971a3c7be7f5c74cd0a5d1
JoeAZTaylor <joetaylor509@yahoo.com>
2024-09-25 10:39:38 +0100

    Test target added

---
+++ RevisionAppTests/Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Tests.swift
+//  RevisionApp
+//
+//  Created by Joe Taylor on 25/09/2024.
+//
+
+import XCTest
+import Combine
+@testable import RevisionApp
+
+class MainViewModelTests: XCTestCase {
+    
+    
+    
+}
