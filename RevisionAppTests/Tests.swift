3ac4cafa46da3a7beb915b1115c034073bc8fb29
JoeAZTaylor <joetaylor509@yahoo.com>
2024-09-25 11:19:58 +0100

    Mock for router added

--- RevisionAppTests/Tests.swift
+++ RevisionAppTests/Tests.swift
@@ -8,9 +8,35 @@
 import XCTest
 import Combine
 @testable import RevisionApp
+import SwiftUI
 
 class MainViewModelTests: XCTestCase {
     
+    func makeSUT() -> MainViewModel {
+        MainViewModel(router: RouterMock(), quizzes)
+    }
+
+    func testTrophyUIBronze(userScoreAsPercentage: Double) {
+        let viewModel = makeSUT()
+        XCTAssertEqual(viewModel.trophyColor(for: userScoreAsPercentage), Color("bronze"))
+    }
     
-    
+}
+
+
+class RouterMock: RouterProtocol {
+    var stack: [Route] = []
+
+    func push(to route: Route) {
+        stack.append(route)
+    }
+
+    func pop() {
+        guard !stack.isEmpty else { return }
+        stack.removeLast()
+    }
+
+    func popToRootView() {
+        stack.removeAll()
+    }
 }
