6b829bc2a8a7478ca26aeb951a18315bede6e42d
JoeAZTaylor <joetaylor509@yahoo.com>
2024-09-26 23:41:26 +0100

    test fixes

--- RevisionAppTests/QuizViewModel.swift
+++ RevisionAppTests/QuizViewModel.swift
@@ -19,18 +19,21 @@ class QuizViewModelTests: XCTestCase {
     func testAnswerIsCorrect() {
         let viewModel = makeSUT()
         XCTAssertEqual(viewModel.score, 0)
-        XCTAssertEqual(viewModel.currentQuestionIndex, 0)
         viewModel.answerIsCorrect()
         XCTAssertEqual(viewModel.score, 1)
-        XCTAssertEqual(viewModel.currentQuestionIndex, 0)
     }
     
     func testAnswerIsIncorrect() {
         let viewModel = makeSUT()
         XCTAssertEqual(viewModel.score, 0)
-        XCTAssertEqual(viewModel.currentQuestionIndex, 0)
-        viewModel.answerIsCorrect()
+        viewModel.answerIsIncorrect()
         XCTAssertEqual(viewModel.score, 0)
+    }
+    
+    func testNextQuestionButton() {
+        let viewModel = makeSUT()
+        XCTAssertEqual(viewModel.currentQuestionIndex, 0)
+        viewModel.nextQuestion()
         XCTAssertEqual(viewModel.currentQuestionIndex, 1)
     }
     
