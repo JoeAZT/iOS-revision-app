35e6a5d3e366013b0b5ffc98f7e9e738928948c9
JoeAZTaylor <joetaylor509@yahoo.com>
2024-11-13 23:14:40 +0000

    Initial UI started for category specific view

--- RevisionApp/MainView/Category/CategoryView.swift
+++ RevisionApp/MainView/Category/CategoryView.swift
@@ -12,35 +12,42 @@ struct CategoryView: View {
     var viewModel: CatergoriesViewModel
     
     var body: some View {
-//        NavigationStack {
-            VStack {
-                ForEach(viewModel.quizzes, id: \.self) { quiz in
-                    VStack {
-                        Text(quiz)
-                    }
+        VStack {
+            ForEach(viewModel.quizzes, id: \.self) { quiz in
+                HStack {
+                    Text(quiz)
+                    Spacer()
+                    Image(systemName: "trophy.fill")
+                        .resizable()
+                        .scaledToFit()
+                        //add trophy for the below
+                        .foregroundColor(.red)
+//                        .foregroundColor(viewModel.trophyColor(for: 100))
+                        .padding(.vertical)
                 }
-                .navigationTitle(viewModel.category)
-//            }
-//            ForEach(viewModel.quizzes.filter { !scoreManager.isQuizCompleted(quiz: $0) }, id: \.self) { quiz in
-//                let bestScore = scoreManager.getScore(for: quiz)
-//                let totalQuestions = scoreManager.getTotalQuestions(for: quiz)
-//                let scorePercentage = totalQuestions > 0 ? (Double(bestScore) / Double(totalQuestions)) * 100 : 0
-//
-//                Button {
-//                    viewModel.didTapNavigateToQuiz(selectedQuiz: quiz)
-//                } label: {
-//                    HStack {
-//                        Text(quiz)
-//                        Spacer()
-//                        Image(systemName: "trophy.fill")
-//                            .resizable()
-//                            .scaledToFit()
-//                            .foregroundColor(viewModel.trophyColor(for: scorePercentage))
-//                            .padding(.vertical)
-//                    }
-//                    .modifier(MainButtonStyle())
-//                }
-//            }
+                .modifier(MainButtonStyle())
+            }
+            .navigationTitle(viewModel.category)
+            //            ForEach(viewModel.quizzes.filter { !scoreManager.isQuizCompleted(quiz: $0) }, id: \.self) { quiz in
+            //                let bestScore = scoreManager.getScore(for: quiz)
+            //                let totalQuestions = scoreManager.getTotalQuestions(for: quiz)
+            //                let scorePercentage = totalQuestions > 0 ? (Double(bestScore) / Double(totalQuestions)) * 100 : 0
+            //
+            //                Button {
+            //                    viewModel.didTapNavigateToQuiz(selectedQuiz: quiz)
+            //                } label: {
+            //                    HStack {
+            //                        Text(quiz)
+            //                        Spacer()
+            //                        Image(systemName: "trophy.fill")
+            //                            .resizable()
+            //                            .scaledToFit()
+            //                            .foregroundColor(viewModel.trophyColor(for: scorePercentage))
+            //                            .padding(.vertical)
+            //                    }
+            //                    .modifier(MainButtonStyle())
+            //                }
+            //            }
         }
     }
 }
