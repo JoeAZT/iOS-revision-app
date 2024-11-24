82ee71dd83acf13537aea4d634e10e68a2fbf52f
JoeAZTaylor <joetaylor509@yahoo.com>
2024-11-24 22:03:07 +0000

    UI fix for quiz cells

--- RevisionApp/MainView/Quiz Menu/QuizMenuCellView.swift
+++ RevisionApp/MainView/Quiz Menu/QuizMenuCellView.swift
@@ -13,35 +13,33 @@ struct QuizMenuCellView: View {
     let viewModel: MainViewModel
     
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .fill(.white)
-                .strokeBorder((viewModel.trophyColor(for: viewModel.getScorePercentage(quiz: quiz)).linearGradient), lineWidth: 4)
-                .padding(.horizontal, 16)
-            HStack {
-                Text("🧠")
-                    .padding(12)
-                    .background(viewModel.trophyColor(for: viewModel.getScorePercentage(quiz: quiz)).linearGradient)
-                    .cornerRadius(12)
-                Text(quiz)
-                Spacer()
-                HStack(spacing: 2) {
-                    if viewModel.getScorePercentage(quiz: quiz) != 0 {
-                        Text("\(viewModel.getScorePercentage(quiz: quiz))")
-                            .bold()
-                        Text("points")
-                            .opacity(0.8)
-                    } else {
-                        Text("0")
-                            .bold()
-                            .opacity(0.5)
-                        Text("points")
-                            .opacity(0.4)
-                    }
+        HStack {
+            Text("🧠")
+                .padding(12)
+                .background(viewModel.trophyColor(for: viewModel.getScorePercentage(quiz: quiz)).linearGradient)
+                .cornerRadius(12)
+            Text(quiz)
+            Spacer()
+            HStack(spacing: 2) {
+                if viewModel.getScorePercentage(quiz: quiz) != 0 {
+                    Text("\(viewModel.getScorePercentage(quiz: quiz))")
+                        .bold()
+                    Text("points")
+                        .opacity(0.8)
+                } else {
+                    Text("0")
+                        .bold()
+                        .opacity(0.5)
+                    Text("points")
+                        .opacity(0.4)
                 }
             }
-            .padding(.horizontal, 32)
-            .padding(.vertical, 16)
         }
+        .padding()
+        .overlay(
+            RoundedRectangle(cornerRadius: 16)
+                .stroke(.offwhite, lineWidth: 4)
+        )
+        .padding(.horizontal, 16)
     }
 }
